90f3268e97819f83e913dbcd38816e5e82a70d3b
Ran <xyfwdy@gmail.com>
2024-04-10 21:53:31 +0800

    新增 api功能

---
+++ js/api.js
@@ -0,0 +1,12 @@
+const api = "http://127.0.0.1:8080";
+//"http://127.0.0.1:8080";
+//"https://api.minelive.top:28080";
+
+function token() {
+	const token = localStorage.getItem('userToken');
+    if (!token) {
+        window.location.href = '../';
+        return;
+    }
+    return token;
+}
