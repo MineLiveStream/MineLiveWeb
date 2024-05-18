e75e93573cca03cdc03806aa68df660cd7144019
Ran <xyfwdy@gmail.com>
2024-05-19 07:00:38 +0800

    fix mdui warn

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+import vue from '@vitejs/plugin-vue'
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith('mdui-')
+        }
+      }
+    })
+  ]
 });
