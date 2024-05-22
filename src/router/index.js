d5acdd35df22ca4467f0efc23205bf8359708f23
Ran <xyfwdy@gmail.com>
2024-05-22 14:41:39 +0800

    fix title error

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,10 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 
-const suffix = " - MineLive";
-
 const routes = [
-    { path: '/', component: () => import('../views/Home.vue'), name: 'Home', meta: {title: '首页' + suffix}},
-    { path: '/stream', component: () => import('../views/Stream.vue'), name: 'Stream', meta: {title: '推流' + suffix}},
-    { path: '/material', component: () => import('../views/Material.vue'), name: 'Material', meta: {title: '素材' + suffix}},
-    { path: '/admin', component: () => import('../views/Admin.vue'), name: 'Admin', meta: {title: '管理' + suffix}},
+    { path: '/', component: () => import('../views/Home.vue'), name: 'Home', meta: {title: '首页'}},
+    { path: '/stream', component: () => import('../views/Stream.vue'), name: 'Stream', meta: {title: '推流'}},
+    { path: '/material', component: () => import('../views/Material.vue'), name: 'Material', meta: {title: '素材'}},
+    { path: '/admin', component: () => import('../views/Admin.vue'), name: 'Admin', meta: {title: '管理'}},
 ]
 
 const router = createRouter({
