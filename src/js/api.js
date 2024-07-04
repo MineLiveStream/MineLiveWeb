b2bd999ca5004bceb4b6a63b56e47f9550ac3678
Ran <xyfwdy@gmail.com>
2024-07-04 20:44:30 +0800

    更新 api地址

--- src/js/api.js
+++ src/js/api.js
@@ -2,7 +2,6 @@ import router from "@/router";
 
 export const api = "https://api.minelive.top";
 //"http://127.0.0.1:8080";
-//"https://api.minelive.top:28080";
 
 export default function token() {
 	const token = localStorage.getItem('userToken');
