6a299082b4569dc0e7bfee41069bba7797a0134e
Ran <xyfwdy@gmail.com>
2025-01-06 16:38:52 +0800

    update vue3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import router from './router';
 import 'mdui';
 import 'mdui/mdui.css';
 import './js/color';
 import { createApp } from 'vue';
+import router from './router';
 import App from './App.vue';
 
 createApp(App).use(router).mount('#app');
+
