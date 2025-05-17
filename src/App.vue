5ea659419125bcf22cded3274f49fc84ccaad5b1
Ran <xyfwdy@gmail.com>
2025-05-17 20:28:20 +0800

    新页面

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup>
-</script>
 <template>
-  <router-view/>
+  <div id="app">
+      <mdui-linear-progress v-if="loading" indeterminate class="fixed top-0 left-0 w-full z-50" />
+      <router-view />
+  </div>
 </template>
+<script setup>
+    import { ref } from 'vue'
+    const loading = ref(false)
+</script>
